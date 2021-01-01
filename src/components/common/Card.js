e911be8946f798e44acf16aa0eaac1d8c97cf409
jblnco.ing <jblnco.ing@gmail.com>
2021-01-01 12:32:31 -0400

    cambio de nombre de los estilos de card

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../styles/Card.css";
+import "../../styles/CardStyles.css";
 
 export const Card = (props) => {
 	const { title, description, point, styles } = props;
